d51189ee32fc1d10653fda962d3ce88a2c513be3
김은채 <a01062534877@gmail.com>
2024-08-30 20:15:07 +0900

    쓸모없는 코드 삭제

--- face_recognition/.ipynb_checkpoints/face_recog-checkpoint.py
+++ face_recognition/.ipynb_checkpoints/face_recog-checkpoint.py
@@ -30,6 +30,7 @@ from facercogAI import IMGclassifier
 # ImageNet 라벨을 가져오는 함수
 data_dir = '분류 폴더가 저장된 디렉토리를 입력하세요.'
 
+#AI integration
 class FaceRecog():
     def __init__(self):
         # Using OpenCV to capture from device 0. If you have trouble capturing
@@ -84,55 +85,6 @@ class FaceRecog():
         imgai_res = self.imageAI.run_prediction(frame, threshold=0.85)
         print(f"AI res: {imgai_res}")
         
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
-
-        # Only process every other frame of video to save time
-        if self.process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            self.face_locations = face_recognition.face_locations(rgb_small_frame)
-            self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
-
-            self.face_names = []
-            for face_encoding in self.face_encodings:
-                # See if the face is a match for the known face(s)
-                distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-                min_value = min(distances)
-
-                # tolerance: How much distance between faces to consider it a match. Lower is more strict.
-                # 0.6 is typical best performance.
-                name = "Unknown"
-                if min_value < 0.65:
-                    index = np.argmin(distances)
-                    name = self.known_face_names[index]
-
-                self.face_names.append(name)
-
-        self.process_this_frame = not self.process_this_frame
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-            
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-            if (name != "Unknown") or (name == "Unknown") :
-                self.log_recognition(name)
-           
-
         return frame
 
     def get_jpg_bytes(self):
